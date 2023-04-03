d22625bf2b7265eddc56261cbddf1b71b6d338ef
kiobc <brayan.calderon3464@utc.edu.ec>
2023-04-03 16:35:56 -0500

    Creacion del forntend del formulario de codigo de token

---
+++ templates/views/login/2faView.php
@@ -0,0 +1,42 @@
+<?php require_once INCLUDES.'inc_header.php'; ?>
+
+<div class="container">
+  <div class="py-5 text-center">
+    <a href="<?php echo URL; ?>"><img src="<?php echo get_image('bee_logo.png') ?>" alt="Bee framework" class="img-fluid" style="width: 150px;"></a>
+    <h2>Autenticacion de 2 factores</h2>
+    <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, ullam.</p>
+  </div>
+
+  <div class="row">
+    <!-- formulario -->
+    <div class="offset-xl-3 col-xl-6 col-12">
+      <div class="card">
+       
+        <div class="card-body">
+          <?php echo Flasher::flash(); ?>
+<div id="verificacion_wrapper" class="text-center"></div>
+          <form id="verificacion_form" class="text-center" method="post" >
+            <?php echo insert_inputs(); ?>
+            <input type="hidden" name="hash" id="hash" value="<?php echo $d->hash;?>">
+
+            <div class="mb-3 row">
+              <div class="offset-xl-3 col-xl-6">
+                <label class="form-label" for="token">Ingresa el codigo de verificacion</label>
+                <input type="text" class="form-control form-control-lg text-center" id="token" name="token" maxlength="6" placeholder="289776" required>
+              <small class="text-muted"><span class="caducidad_token" data-caducidad="<?php echo $d->caducidad; ?>">123</span><span class="caducidad_texto">segundos restantes</span></small>
+            </div>
+
+            <button class="btn btn-primary btn-lg" type="submit"><i class="fas fa-fingerprint fa-fw"></i>Validar cuenta</button>
+
+          </form>
+        </div>
+<div class="card-footer">
+<small class="text-muted float-end">¿No recibiste el mensaje? Volver a enviar <a class="text-decoration-none reenviar_codigo" href="#">aquí</a>.</small>
+</div>     
+ </div>
+    </div>
+  </div>
+</div>
+
+<?php require_once INCLUDES.'inc_footer_v2.php'; ?>
+
